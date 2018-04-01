5ec201cb782686eb9fb5f2222313b5084e762edf
shan081251 <chenyishan3@163.com>
2018-04-01 19:06:03 +0800

    one commit

---
+++ demo/src/main/java/org/trimps/helloworld/demo/DemoController.java
@@ -0,0 +1,19 @@
+package org.trimps.helloworld.demo;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class DemoController {
+
+	 @RequestMapping("/")
+	    @ResponseBody
+	    String home() {
+	        return "Hello World!";
+	    }
+
+	
+}
